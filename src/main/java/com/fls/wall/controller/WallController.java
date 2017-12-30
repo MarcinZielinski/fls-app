00a3face2e45cd52ebc0050b4937d821b6c8a0e1
somas3k <sikorka1901@gmail.com>
2017-12-30 17:42:37 +0100

    name fix

--- src/main/java/com/fls/wall/controller/WallController.java
+++ src/main/java/com/fls/wall/controller/WallController.java
@@ -5,7 +5,7 @@ import com.fls.wall.Wall;
 /**
  * Created by Marcin on 2017-12-12.
  */
-public abstract class WallContoller {
+public abstract class WallController {
     private Wall model;
     abstract void initialize();
     abstract void updateModel();
